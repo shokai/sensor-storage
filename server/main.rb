4f8b9eea40b5441b525cc2da0b2a9b93b38db138
Sho Hashimoto <hashimoto@shokai.org>
2010-04-25 18:47:08 +0900

    /somestring/でdbを新規作成

--- server/main.rb
+++ server/main.rb
@@ -8,57 +8,69 @@ require 'json'
 require 'tokyocabinet'
 include TokyoCabinet
 
-before do
+@@dbdir = 'db'
+
+def db_open(dbname='/')
+  dbname = dbname.to_s.gsub(/\//, '_')
   @hdb = HDB.new
-  @hdb.open('sensordata.tch', HDB::OWRITER|HDB::OCREAT)
+  Dir.mkdir(@@dbdir) if !File.exists?(@@dbdir)
+  @hdb.open("#{@@dbdir}/#{dbname}.tch", HDB::OWRITER|HDB::OCREAT)  
 end
 
 after do
-  @hdb.close
+  @hdb.close if @hdb
 end
 
-get '/' do
-  redirect './readme'
+get '*/' do
+  path = "../" * params[:splat].first.count('/')
+  redirect "#{path}./readme"
 end
 
-get '/keys' do
-  keys = @hdb.keys.reverse[0...10000]
-  keys.to_json
+get '*/keys' do
+  db_open(params[:splat])
+  @hdb.keys.reverse[0...10000].to_json
 end
 
-get '/count' do
+get '*/count' do
+  db_open(params[:splat])
   {"count", @hdb.rnum}.to_json
 end
 
-get '/last' do
+get '*/last' do
+  db_open(params[:splat])
   key = @hdb.keys.last
   {key, @hdb[key]}.to_json
 end
 
-get '/recent' do
+get '*/recent' do
+  db_open(params[:splat])
   @hdb.keys.reverse[0...10].map{|k| {k, @hdb[k]} }.to_json
 end
 
-get '/recent/:num' do
+get '*/recent/:num' do
+  db_open(params[:splat])
   num = [params[:num].to_i, 1000].min
   @hdb.keys.reverse[0...num].map{|k| {k, @hdb[k]}}.to_json
 end
 
-get '/:tc_key' do
+get '*/:tc_key' do
+  db_open(params[:splat])
   key = params[:tc_key]
   {key, @hdb[key]}.to_json
 end
 
-post '/' do
+post '*/' do
+  db_open(params[:splat])
   now = Time.now
   key = "#{now.to_i}_#{now.usec}"
   @hdb.put(key, params.to_json)
   {key, @hdb[key]}.to_json
 end
 
-delete '/:tc_key' do
+delete '*/:tc_key' do
 key = params[:tc_key]
   v = @hdb[params[:tc_key]]
   @hdb.out(params[:tc_key])
   {key,v}.to_json
 end
+
